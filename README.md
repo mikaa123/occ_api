c4592aa86998b2b0dd1bc241e38b8061ed52f031
Michael Sokol <mikaa123@gmail.com>
2014-05-01 19:09:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Environment variables
+
+They are stored in `.env`, which is read by foreman.
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+	passport = require('passport'),
+	http = require('http'),
+	config = require('./lib/config/config'),
+	app = express();
+
+app.config = config;
+
+require('./lib/config/passport')(app, passport);
+require('./lib/config/express')(app, express, passport);
+
+var server = http.createServer(app)
+  .listen(app.get('port'), config.server.hostname, function (err) {
+
+    if (err) {
+      return console.trace(err);
+    }
+
+    console.log("\n✔ Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
+
+  });
+
+server.on('error', function (err) {
+  console.error('✗ '+ app.get('port') + err);
+  // TODO: do something with the error
+});
+
+module.exports = app;
