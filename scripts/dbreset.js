f19afd708eb01b2e4b877c1a6d547063d5142e99
Michael Sokol <mikaa123@gmail.com>
2014-06-11 11:22:09 +0200

    some settings for heroku

--- scripts/dbreset.js
+++ scripts/dbreset.js
@@ -3,6 +3,18 @@ var mongoFixtures = require('mongo_fixtures'),
 	ObjectId = require('mongoose').Types.ObjectId;
 
 mongoFixtures('mongodb://localhost/occ', {
+	'User': [
+		{
+			'_id': '5395c5e6a8cac63681117607',
+			'username': "userfoo",
+			'email': "foo@bar.com",
+			'hashed_password': "83f6953ca87e0fe43c9c0ea517ec1dfe926bde88",
+			'salt' : "972512995865"
+		},
+		{
+			'username': "bar",
+		}
+	],
 	'Issue': [
 		{
 			_id: "5396fe86c362ffb556d23c3b",
